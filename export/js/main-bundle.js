86bf1303f881b0dd9e179d4e3ca9872ade3bf630
Brian Graves <bgraves@degdigital.com>
2017-05-23 11:31:09 -0500

    export only

--- export/js/main-bundle.js
+++
@@ -1,7 +0,0 @@
-System.registerDynamic("main.js", [], false, function ($__require, $__exports, $__module) {
-  var _retrieveGlobal = System.get("@@global-helpers").prepareGlobal($__module.id, null, null);
-
-  (function ($__global) {})(this);
-
-  return _retrieveGlobal();
-});
