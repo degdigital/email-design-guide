dadd32f1407f2a0cd938f23c7053a590a213d1cd
Brian Graves <bgraves@degdigital.com>
2017-05-31 17:23:47 -0500

    added support grid

--- grunt/tasks/patterns/copy.js
+++ grunt/tasks/patterns/copy.js
@@ -21,21 +21,6 @@ module.exports = function(grunt) {
 				        		fileName = src.substr(lastDigit + 3);
 				            return dest + fileName;
 				        }
-				    },
-				    {
-				        "expand": true,
-				        "cwd": "<%= activeTheme.public.assetPaths.patterns %>",
-				        "src": ["**/*-components-*-*.markup-only.html", "!**/*-components-*-_*.markup-only.html"],
-				        "dest": path.normalize("<%= activeTheme.export.assetPaths.patterns %>/components/"),
-				        "flatten": true,
-				        "filter": 'isFile',
-				        "rename": function(dest, src) {
-				        	let digits = src.match(/\d+/g),
-				        		lastDigit = src.lastIndexOf(digits[digits.length - 1]),
-				        		fileName = src.substr(lastDigit + 3).replace('.markup-only', '');
-
-				        	return dest + fileName;
-				        }
 				    }
 			    ],
 			    "options": {
